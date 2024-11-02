b42f909741085245938ddc498c4e25b57a219297
Sotaneum <gnyontu39@gmail.com>
2024-11-02 21:23:18 +0900

    fix. 확장성 대응

--- pg/log.go
+++ pg/log.go
@@ -8,7 +8,8 @@ import (
 	"github.com/schedule-job/schedule-job-database/core"
 )
 
-func (p *PostgresSQL) InsertRequestLog(jobID string, data core.RequestTypePayload) error {
+func (p *PostgresSQL) InsertRequestLog(jobID string, payload interface{}) error {
+	data := payload.(core.RequestTypePayload)
 	_, err := p.usePostgresSQL(func(client *pgx.Conn, ctx context.Context) (result interface{}, err error) {
 		return client.Exec(ctx, "INSERT INTO request_logs (job_id, status, request_url, request_method, request_headers, request_body, response_headers, response_body, response_status_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", jobID, data.Status, data.RequestUrl, data.RequestMethod, data.RequestHeaders, data.RequestBody, data.ResponseHeaders, data.ResponseBody, data.ResponseStatusCode)
 	})
